64bbc37253aa9aad7ae981d33d6997218564372f
arkin0x <99223753+arkin0x@users.noreply.github.com>
2023-06-14 14:58:02 -0500

    map state

--- src/components/YondarMap.tsx
+++ src/components/YondarMap.tsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import { ViewState} from 'react-map-gl'
 import Map from 'react-map-gl'
 import 'mapbox-gl/dist/mapbox-gl.css'
 
@@ -6,16 +7,21 @@ export const YondarMap = () => {
   const [longitude, setLongitude] = useState<number>(-80)
   const [latitude, setLatitude] = useState<number>(0)
   const [zoom, setZoom] = useState<number>(1)
+
+  function setViewState(viewState: ViewState) {
+    setLongitude(viewState.longitude)
+    setLatitude(viewState.latitude)
+    setZoom(viewState.zoom)
+  }
   return (
     <Map
       mapboxAccessToken='pk.eyJ1IjoiaW5ub3ZhdGFyIiwiYSI6ImNrZW0wZWwybjAyYnUyeW85bW4za3dtNDUifQ.xa1351lJ3mZH1P9c1fFsGg'
-      initialViewState={{
-        longitude,
-        latitude,
-        zoom
-      }}
-      style={{ width: '100%', height: '50vh', borderRadius: '1rem', marginTop: '1rem' }}
-      mapStyle='mapbox://styles/mapbox/streets-v11'
+      longitude={longitude}
+      latitude={latitude}
+      zoom={zoom}
+      style={{ maxWidth: '100%', height: '50vh', borderRadius: '1rem', marginTop: '1rem' }}
+      onMove={e => setViewState(e.viewState)}
+      mapStyle='mapbox://styles/innovatar/ckg6zpegq44ym19pen438iclf'
     />
   )
 }
