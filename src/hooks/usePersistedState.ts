3fac7f637376abaacd5d152e6b73c9be70eb3ff2
arkin0x <99223753+arkin0x@users.noreply.github.com>
2023-08-23 16:34:04 -0500

    catch error on usePersistedState

--- src/hooks/usePersistedState.ts
+++ src/hooks/usePersistedState.ts
@@ -3,7 +3,12 @@ import React from 'react'
 export default function usePersistedState<T>(key: string, defaultValue: T): [T, React.Dispatch<React.SetStateAction<T>>] { // use unknown here https://typescript-eslint.io/rules/no-unsafe-return/#examples
   const [state, setState] = React.useState<T>(() => {
     const persistedState = localStorage.getItem(key)
-    return persistedState ? JSON.parse(persistedState) : defaultValue
+    try {
+      return persistedState ? JSON.parse(persistedState) : defaultValue
+    } catch (error) {
+      console.error(error)
+      return defaultValue
+    }
   })
   React.useEffect(() => {
     window.localStorage.setItem(key, JSON.stringify(state))
