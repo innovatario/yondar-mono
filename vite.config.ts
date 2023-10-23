3e57a3eb7df578bf02cd5a8dc51266bdbdc71525
arkin0x <99223753+arkin0x@users.noreply.github.com>
2023-10-23 16:32:44 -0500

    added autoprefixer for ios

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import autoprefixer from 'autoprefixer'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+  ],
+  css: {
+    postcss: {
+      plugins: [
+        autoprefixer(),
+      ],
+    },
+  },
 })
