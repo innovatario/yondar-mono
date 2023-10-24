927e3f8facb96f900b748ff3c968a567ce4e42e6
arkin0x <99223753+arkin0x@users.noreply.github.com>
2023-10-24 13:42:32 -0500

    improve faq. add boop.

--- src/components/FAQ.tsx
+++ src/components/FAQ.tsx
@@ -1,5 +1,9 @@
 import { WavyText } from "./WavyText"
 import "../scss/FAQ.scss"
+import { GeoChatExampleButton } from "./GeoChatExampleButton"
+import { FaComments } from "react-icons/fa"
+import { SignUpButton } from "./SignUpButton"
+import { AddExampleButton } from "./AddExampleButton"
 
 const Goolag = () => (
   <>
@@ -11,6 +15,8 @@ const Goolag = () => (
     <span style={{ color: "red" }}>g</span>
   </>
 )
+
+const Geochat = () => <span style={{color: "#d3baf3"}}><WavyText text="Geochat"/></span>
 
 export const FAQ = () => {
   return (
@@ -25,50 +31,90 @@ export const FAQ = () => {
         <span role="img" aria-label="thumbs up">
           👍
         </span>{" "}
-        You can publish your business, event, or attraction on a decentralized global map for the world to see! 
+        You can publish places on a decentralized map for the world to see!
+      </p>
+      <p className="receive">
+        Just tap the map 🗺️ and hit this button:
+        <br/>
+        <br/>
+        <AddExampleButton/>
       </p>
       <p className="send">Is it like putting a business on <Goolag/> Maps?</p>
       <p className="receive">
         Yeah, but without the <Goolag />!
-        <br />
-        <br />
-        You can also see Places owned by the people you follow on <a href="https://heynostr.com" target="_blank" rel="noopener noreferrer">nostr</a>.
-        <br />
-        <br />
-        More exciting features are coming too, like end-to-end encrypted
-        location sharing!
       </p>
-
       <p className="send">
-        That sounds fun! Why do I need to sign in? ✍
+        Nice!&nbsp;
+        <span role="img" aria-label="smile">
+          😂🤙
+        </span>
       </p>
 
       <p className="receive">
-        Yondar is a social map experience, but to interact you need to have a <a href="https://heynostr.com" target="_blank" rel="noopener noreferrer">nostr</a> identity, which is just a really long unguessable number.
+        The Yondar map is a social place too 😆📢
+      </p>
+      <p className="receive">
+        <GeoChatExampleButton/>
+        <br/>
+        <Geochat/> lets you share posts or chat with others based on your geographic area! 🌎
+      </p>
+      <p className="send">
+        That sounds cool, but what about privacy? 🤔
+      </p>
+      <p className="receive">
+        Since <Geochat/> is based on regions, no precise personal location is shared. Also, chat is optional!
+      </p>
+      <p className="send">
+        Introvert friendly! 🤓
+      </p>
+      <p className="receive">
+        You can also see Places owned by your friends 🤗 and follows 🫡 on <a href="https://heynostr.com" target="_blank" rel="noopener noreferrer" style={{ color: '#ae6cff'}}>nostr</a>! 🫂 
+      </p>
+      <p className="send">
+        TOASTR? 😮🍞🔥
+      </p>
+      <p className="receive">
+        No, NOSTR! 🤦‍♂️ It's a new <a href="https://heynostr.com" target="_blank" rel="noopener noreferrer" style={{ color: '#ae6cff'}}>social internet</a> that makes new kinds of apps possible!
+      </p>
+      <p className="send">
+        That sounds fun! Do I need an account? ✍
+      </p>
+      <p className="receive">
+        Boom:&nbsp;
+        <SignUpButton/>
         <br/>
         <br/>
-        Using magical mathematics ✨🧙‍♂️ you can create Places that you have provable ownership of, and nobody can fake it or take it away from you!
+        It's all anonymous 🎭 unless you want to fill out your profile.
+        <br/>
+        <br/>
+        With one click you can have a new account and join the fun!
+        <br/>
+        <br/>
+        Or sign in if you're a nostr user already.
       </p>
 
       <p className="send">
-        Wow that's amazing! How do I get a nostr id? 🕵️‍♂️
+        Wow that's amazing! What does my account do? 💪
       </p>
 
       <p className="receive">
-        Yondar can generate one for you. Then you can take your identity to any other <a href="https://nostrapps.com" target="_blank" rel="noopener noreferrer" >nostr-compatible app</a> and use it there too! That's the superpower of the nostr protocol!
+        It allows you to create places 📍, edit them, and post in <Geochat/>
+        <br/>
+        <br/>
+        Then you can take your identity to any other <a href="https://nostrapps.com" target="_blank" rel="noopener noreferrer" >nostr-compatible app</a> and use it there too! That's the superpower of the nostr protocol!
         <br/>
         <br/>
         If you already have a nostr id, you can use it through a browser extension like <a href="https://getalby.com" target="_blank" rel="noopener noreferrer">Alby on Desktop</a>, <a href="https://apps.apple.com/us/app/nostore/id1666553677" target="_blank" rel="noopener noreferrer">Nostore for iOS</a>, or <a href="https://github.com/nostrband/nostr-universe/releases/tag/v0.1.4" target="_blank" rel="noopener noreferrer">Spring browser for Android</a>.
       </p>
 
       <p className="send">
-        What is my location used for? 🤔 Is Yondar private and secure?
+        What is my location used for? 🤔
       </p>
       <p className="receive">
-        Yes! Your personal GPS location simply shows where you are on the map in relation to other Places. It can be helpful if you want to put a new Place right where you are!
+        Your GPS location simply shows where you are on the map in relation to other places. It can be helpful if you want to put a new place right where you are! 🎯
         <br/>
         <br/>
-        Your location is not transmitted, and our code will soon be open-sourced to prove it. <a href="https://innovatar.io" target="_blank" rel="noopener noreferrer">We are</a> regular people who value our privacy, so we built Yondar to do the same for you. ✅
+        Your location is not transmitted, and our code is <a href="https://github.com/innovatar/yondar-mono.git" target="_blank" rel="noopener noreferrer">open-sourced</a> to prove it. <a href="https://innovatar.io" target="_blank" rel="noopener noreferrer">We are</a> regular people who value our privacy, so we built Yondar to do the same for you. ✅
       </p>
 
       <p className="send">
@@ -100,33 +146,9 @@ export const FAQ = () => {
         safety!
       </p>
 
-      <p className="send">
-        Does Yondar work indoors?{" "}
-        <span role="img" aria-label="house">
-          🏡
-        </span>
-      </p>
-      <p className="receive">
-        Yondar is meant for outdoor use{" "}
-        <span role="img" aria-label="sun">
-          🌞
-        </span>
-        <span role="img" aria-label="cloud">
-          ☁
-        </span>{" "}
-        GPS technology has trouble when there is stuff between you and the sky{" "}
-        <span role="img" aria-label="satelite dish">
-          📡
-        </span>
-        <span role="img" aria-label="satelite">
-          🛰
-        </span>{" "}
-        so keep that sky in view!
-      </p>
-
       <p className="send">What is <a href="https://yondar.me">https://yondar.me</a>? Is it a different app?</p>
 
-      <p className="receive">Yondar.me started out as a centralized web2 PWA, but go.yondar.me is our new decentralized and end-to-end encrypted version of Yondar built on the nostr protocol!
+      <p className="receive">Yondar.me started out as a centralized web2 PWA, but go.yondar.me is our new decentralized version of Yondar built on the nostr protocol!
       <br/>
       <br/>
       Once go.yondar.me reaches feature parity with yondar.me, we will shut down the old Yondar and replace it with the new nostr-powered Yondar! Nostr is the future and we are excited to be a part of it! 🚀
---
+++ src/components/GeoChatExampleButton.tsx
@@ -0,0 +1,38 @@
+import {useState} from 'react'
+import { FaComments } from 'react-icons/fa'
+
+export const GeoChatExampleButton = () => {
+  const [boop, setBoop] = useState<number|undefined>()
+  const handleClick = () => {
+    if (typeof boop === 'undefined')
+      setBoop( setTimeout( () => {
+          setBoop(undefined)
+        }, 1000)
+      )
+  }
+  return (
+    <>
+      <button onClick={handleClick} className={`component-example-geochatbutton`} style={{
+        alignItems: 'center',
+        backgroundColor: '#7200ff',
+        borderRadius: '50%',
+        border: '2px solid #4707b1',
+        bottom: '2rem',
+        boxShadow: '5px 5px 5px 0px rgba(0,0,0,0.5)',
+        color: '#ffffff',
+        cursor: 'pointer',
+        display: 'flex',
+        height: '50px',
+        width: '50px',
+        justifyContent: 'center',
+        outline: 'none',
+        padding: '7px',
+      }}>
+        <FaComments size={44} />
+      </button>
+      { boop ? <div className="component-shared" style={{width: '50px', marginLeft: '1rem', marginTop: '-3rem', cursor: 'pointer', fontSize: '30px', animation: 'floatUp 1s ease-in-out'}}>
+        Boop!
+      </div> : null }
+    </>
+  )
+}
