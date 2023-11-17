ae9d6ebbd3fc2195f27f3cec28091493a4ac0234
EricStrohmaier <eric.strohmaier00@gmail.com>
2023-11-17 06:44:51 -0500

    made design more responsive added external link

--- src/components/MyAccount.tsx
+++ src/components/MyAccount.tsx
@@ -1,11 +1,8 @@
 import { useContext, useState } from "react"
 import { IdentityContext } from "../providers/IdentityProvider"
 import { IdentityContextType } from "../types/IdentityType"
-import defaultDisplayImage from '../assets/default-display-image.png'
-import defaultBanner from '../assets/default-banner.png'
-// import '../scss/Dashboard.scss'
-import '../scss/MyAccount.scss'
-import { Nip05Verifier } from "./Nip05Verifier"
+import { nip19 } from "nostr-tools"
+import { MyAccountProfile } from "./MyAccountProfile"
 
 export const MyAccount = () => {
   const {identity} = useContext<IdentityContextType>(IdentityContext)
@@ -15,54 +12,19 @@ export const MyAccount = () => {
     event.stopPropagation()
     setHandleProfile(!handleProfile)
   }
-  const displayImage = identity?.picture && identity.picture !== 'unknown' ? identity.picture : defaultDisplayImage
-
-  const background = identity?.banner && identity.banner !== 'unknown' ? identity.banner : defaultBanner
-  const backgroundStyle = {
-    backgroundImage: `url(${background})`,
-    backgroundRepeat: 'no-repeat',
-    backgroundSize: 'cover',
-    backgroundPosition: 'center',
+  const handleLink = (event: React.MouseEvent<HTMLElement>) => {
+    event.preventDefault()
+    const npub = nip19.npubEncode(identity.pubkey)
+    window.open(`https://njump.me/${npub}`, '_blank', 'noopener noreferrer')
   }
-
-  const displayName = identity?.display_name && identity?.display_name !== 'unknown' ? identity.display_name : identity?.displayName && identity?.displayName !== 'unknown' ? identity.displayName : 'unknown'
-
-  
-  const $profile = (
-    <>
-      <div className="profileBox">
-        <div className="flexcol align-center background-picture" style={backgroundStyle}>
-          <img className="profile-picture" src={displayImage} alt={`${displayName}'s profile picture`}/>&nbsp;
-        </div>
-          <div className=" profile-name-box">
-            <div className="profile-name">{displayName}</div>
-            {/* TODO: add external link to "see full profile" Link to njump */}
-            <Nip05Verifier pubkey={identity?.pubkey} nip05Identifier={identity?.nip05} />
-          </div>
-        <div className="about full">
-        List of Places I have added:
-        <br/>
-        List of Reviews I have wrote:
-        <br/>
-        List of Reviews I have liked:
-        </div>
-      </div>
-      <br/>
-    </>
-  )
 
   return (
     <>
-    { handleProfile ? $profile : null }
-    <button onClick={handleClick}>View My Account</button>
+    { handleProfile ? <MyAccountProfile/> : null }
+    { handleProfile ? <button onClick={handleLink}>Go to Full Account</button>: <button onClick={handleClick}>View My Account</button> }
     <br/>
     </>
   )
   
 
 }
-
-
-
-  // const npub = nip19.npubEncode(identity.pubkey)
-    // window.open(`https://njump.me/${npub}`, '_blank', 'noopener noreferrer')
---
+++ src/components/MyAccountProfile.tsx
@@ -0,0 +1,53 @@
+import { useContext } from "react"
+import { IdentityContext } from "../providers/IdentityProvider"
+import { IdentityContextType } from "../types/IdentityType"
+import defaultDisplayImage from '../assets/default-display-image.png'
+import defaultBanner from '../assets/default-banner.png'
+import '../scss/MyAccountProfile.scss'
+import { Nip05Verifier } from "./Nip05Verifier"
+
+export const MyAccountProfile = () => {
+  const {identity} = useContext<IdentityContextType>(IdentityContext)
+  
+  const displayImage = identity?.picture && identity.picture !== 'unknown' ? identity.picture : defaultDisplayImage
+
+  const background = identity?.banner && identity.banner !== 'unknown' ? identity.banner : defaultBanner
+  const backgroundStyle = {
+    backgroundImage: `url(${background})`,
+    backgroundRepeat: 'no-repeat',
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+  }
+  const displayName = identity?.display_name && identity?.display_name !== 'unknown' ? identity.display_name : identity?.displayName && identity?.displayName !== 'unknown' ? identity.displayName : 'unknown'
+  
+  const $profile = (
+    <>
+      <div className="profileBox">
+        <div className="flexcol align-center background-picture" style={backgroundStyle}>
+          <img className="profile-picture" src={displayImage} alt={`${displayName}'s profile picture`}/>&nbsp;
+        </div>
+          <div className=" profile-name-box">
+            <div className="profile-name">{displayName}</div>
+            {/* TODO: add external link to "see full profile" Link to njump */}
+            <Nip05Verifier pubkey={identity?.pubkey} nip05Identifier={identity?.nip05} />
+          </div>
+        <div className="about full">
+        List of Places I have added:
+        <br/>
+        List of Reviews I have wrote:
+        <br/>
+        List of Reviews I have liked:
+        </div>
+      </div>
+      <br/>
+    </>
+  )
+
+  return (
+    <>
+        { $profile }
+    </>
+  )
+  
+
+}
